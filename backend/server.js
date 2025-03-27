6f8854ee83ffc7c206cf464b2548ec7ae8de218e
vaibhav-dwivedi-auriga <vaibhav.dwivedi@aurigait.com>
2025-03-27 14:41:53 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: [
-    ['Access-Control-Allow-Origin', 'https://aws-facerekog-react-ljtj.vercel.app/'],
+    ['Access-Control-Allow-Origin', 'https://aws-facerekog-react-ljtj.vercel.app'],
     ['Access-Control-Allow-Credentials', 'true'],
     ['Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'],
     ['Access-Control-Allow-Headers', 'Content-Type, Authorization'],
